e3472f3c4de6f2d97461e32e548c9e5960cdf914
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2022-05-13 12:08:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 from Controller import Controller
 from functions import parse_data
+import PySimpleGUI as Sg
 
 
 def main():
     instructions = parse_data("instructions_20000_20.xml")
     controller = Controller(instructions)
-    jiffy, instruction, page_table, ram = controller.one_instruction()
-    print("Jiffy:", jiffy)
-    print("Instruction:", instruction)
-    for page in page_table:
-        print(page)
-    for ram_page in ram:
-        print(ram_page)
-    print('-----------------------------------------------------')
 
-    for _ in range(1500):
-        jiffy, instruction, page_table, ram = controller.one_instruction()
-
-    jiffy, instruction, page_table, ram = controller.one_instruction()
-    print("Jiffy:", jiffy)
-    print("Instruction:", instruction)
-    for page in page_table:
-        print(page)
-    for ram_page in ram:
-        print(ram_page)
-    print('-----------------------------------------------------')
+    layout = [[Sg.Text("Besturingssystemen")]], [Sg.Button("Run 1")], [Sg.Button("Run all")]
+    window = Sg.Window("Demo", layout)
+    while True:
+        event, values = window.read()
+        if event == "Run 1":
+            Sg.Print(controller.one_instruction())
+        elif event == "Run all":
+            print("alle processen")
+        elif event == Sg.WIN_CLOSED:
+            break
 
 
 if __name__ == "__main__":
     main()
+
